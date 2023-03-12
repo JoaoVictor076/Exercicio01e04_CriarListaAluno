65f5d4d2a5f03d7116ae3b223e7e5a523c8b0ca2
JoaoVictor076 <115574299+JoaoVictor076@users.noreply.github.com>
2023-03-12 19:07:36 -0300

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Exercicio 1 e 4
+namespace ListaAluno
+{
+    internal class Aluno
+    {
+
+        public string ra;
+        public string nome;
+        public decimal notaProva;
+        public decimal notaTrabalho;
+        public decimal notaProva2;
+        public decimal notaTrabalho2;
+        decimal notaFinal {get; set;}
+        decimal notaTotal { get; set;}
+
+
+        public void CalcularMedia()
+        {
+            notaTotal = (notaProva + notaTrabalho + notaProva2 + notaTrabalho2);
+            notaFinal = notaTotal / 2;
+        }
+        public bool CalcularNotaFinal()
+        {
+
+            decimal valorPassar = 14;
+            if ((notaTotal) < valorPassar)
+
+            {
+                Console.WriteLine("Aluno Reprovado!");
+                Console.WriteLine("Nota necessaria para prova final: " + (12 - (notaFinal)));
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Aluno Aprovado!");
+                return false;
+            }
+
+        }
+        public void ImprimirNotaFinal()
+        {
+            Console.WriteLine($"Nota Bimestre: {(notaTotal).ToString("N2")}");
+        }
+        public void ReceberDados()
+        {
+            Console.Write("Digite o RA do aluno: ");
+            ra = Console.ReadLine();
+            Console.Write("Digite o Nome do aluno: ");
+            nome = Console.ReadLine();
+            Console.Write("Digite o valor da Prova: ");
+            notaProva = Decimal.Parse(Console.ReadLine());
+            Console.Write("Digite o valor da Prova 2: ");
+            notaProva2 = Decimal.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do Trabalho: ");
+            notaTrabalho = Decimal.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do Trabalho 2: ");
+            notaTrabalho2 = Decimal.Parse(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Exercicio 1 e 4
+using ListaAluno;
+Console.WriteLine("DIgite a quantidade de alunos");
+int quantidadeAlunos = int.Parse(Console.ReadLine());
+
+List<Aluno> listaAlunos = new List<Aluno>();
+for (int i = 0; i < quantidadeAlunos; i++)
+{
+    Console.WriteLine("\nDigite numero de alunos{0}", i + 1);
+
+    Aluno aluno = new Aluno();
+    aluno.ReceberDados();
+    aluno.CalcularMedia();
+    listaAlunos.Add(aluno);
+
+}
+
+Console.WriteLine("\n Notas Finais");
+foreach (Aluno aluno in listaAlunos)
+{
+    aluno.CalcularNotaFinal();
+    aluno.ImprimirNotaFinal();
+}
+
+Console.ReadLine();
